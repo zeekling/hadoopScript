f954c539806d34e1a0a1cf4ece37c0b7e5a0b8c2
zeekling <lingzhaohui@zeekling.cn>
2022-09-18 15:59:59 +0800

    update

--- hadoop/build.sh
+++ hadoop/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash 
 
-docker pull harisekhon/hadoop 
+# docker pull zeekling/hadoop 
 
 docker stop hadoop 
 
 docker rm hadoop
 
 docker run -dit --name hadoop --privileged=true \
-  -p 8042 -p 8088 -p 19888 -p 50070 -p 50075 harisekhon/hadoop
+  -p 8042 -p 8088 -p 19888 -p 50070 -p 50075 zeekling/hadoop
 
